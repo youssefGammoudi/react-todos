50c0c7790cd369da3cecbcfc20c984572e543dea
Youssef Gammoudi <gama108jose@gmail.com>
2023-06-07 23:27:14 +0100

    add todoitem

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,12 +1,6 @@
 import { useState } from "react";
 import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import Checkbox from "@mui/material/Checkbox";
-import IconButton from "@mui/material/IconButton";
-import DeleteIcon from "@mui/icons-material/Delete";
+import TodoItem from "./TodoItem";
 const initialTodos = [
   { id: 1, text: "walk the dog", completed: false },
   { id: 3, text: "walk the cat", completed: false },
@@ -18,34 +12,12 @@ export default function TodoList() {
   const [todos, setTodos] = useState(initialTodos);
   return (
     <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-      {todos.map((todo) => {
-        const labelId = `checkbox-list-label-${todo.id}`;
-        return (
-          <ListItem
-            key={todo.id}
-            secondaryAction={
-              <IconButton edge="end" aria-label="delete">
-                <DeleteIcon />
-              </IconButton>
-            }
-            disablePadding
-          >
-            <ListItemButton role={undefined} dense>
-              <ListItemIcon>
-                <Checkbox
-                  edge="start"
-                  checked={todo.completed}
-                  tabIndex={-1}
-                  disableRipple
-                  inputProps={{ "aria-labelledby": labelId }}
-                />
-              </ListItemIcon>
-              <ListItemText id={labelId} primary={todo.text} />
-            </ListItemButton>
-          </ListItem>
-        );
-      })}
-      {/* })} */}
+      {/* {todos.map((todo) => {
+        return <TodoItem todo={todo} key={todo.id} />;
+      })} */}
+      {todos.map((todo) => (
+        <TodoItem todo={todo} key={todo.id} />
+      ))}
     </List>
   );
 }
