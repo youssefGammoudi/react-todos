3ae294b2ab5cdbfd095a5ea273336d8cf16706b6
Youssef Gammoudi <gama108jose@gmail.com>
2023-06-09 23:54:02 +0100

    add functionalities to todoList

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -6,7 +6,7 @@ import Checkbox from "@mui/material/Checkbox";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
 
-export default function TodoItem({ todo, remove }) {
+export default function TodoItem({ todo, remove, toggle }) {
   const labelId = `checkbox-list-label-${todo.id}`;
   const removeTodo = () => {
     remove(todo.id);
@@ -28,6 +28,7 @@ export default function TodoItem({ todo, remove }) {
             tabIndex={-1}
             disableRipple
             inputProps={{ "aria-labelledby": labelId }}
+            onChange={toggle}
           />
         </ListItemIcon>
         <ListItemText id={labelId} primary={todo.text} />
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,18 +1,50 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import List from "@mui/material/List";
 import TodoItem from "./TodoItem";
-const initialTodos = [
-  { id: 1, text: "walk the dog", completed: false },
-  { id: 3, text: "walk the cat", completed: false },
-  { id: 4, text: "walk the fish", completed: true },
-  { id: 5, text: "walk the chicken", completed: true },
-];
+import TodoForm from "./TodoForm";
+
+// const initialTodos = [
+//   { id: 1, text: "walk the dog", completed: false },
+//   { id: 3, text: "walk the cat", completed: false },
+//   { id: 4, text: "walk the fish", completed: true },
+//   { id: 5, text: "walk the chicken", completed: true },
+// ];
+const getInitialData = () => {
+  const data = JSON.parse(localStorage.getItem("todos"));
+  if (!data) return [];
+  return data;
+};
 
 export default function TodoList() {
-  const [todos, setTodos] = useState(initialTodos);
+  const [todos, setTodos] = useState(getInitialData);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   const removeTodo = (id) => {
     setTodos((prevTodos) => {
       return prevTodos.filter((t) => t.id !== id);
+    });
+  };
+
+  const toggleTodo = (id) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        } else {
+          return todo;
+        }
+      });
+    });
+  };
+  const addTodo = (text) => {
+    setTodos((prevTodos) => {
+      return [
+        ...prevTodos,
+        { text: text, id: crypto.randomUUID(), completed: false },
+      ];
     });
   };
   return (
@@ -21,8 +53,14 @@ export default function TodoList() {
         return <TodoItem todo={todo} key={todo.id} />;
       })} */}
       {todos.map((todo) => (
-        <TodoItem todo={todo} key={todo.id} remove={removeTodo} />
+        <TodoItem
+          todo={todo}
+          key={todo.id}
+          remove={removeTodo}
+          toggle={() => toggleTodo(todo.id)}
+        />
       ))}
+      <TodoForm addTodo={addTodo} />
     </List>
   );
 }
