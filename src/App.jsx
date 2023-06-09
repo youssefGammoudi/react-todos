06b0c29b3e374610625205270a2b427367426994
Youssef Gammoudi <gama108jose@gmail.com>
2023-06-09 23:59:39 +0100

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import "./App.css";
 import TodoList from "./TodoList";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <CssBaseline />
       <h1>Todos</h1>
       <TodoList />
